f294fa818be672534258cb364677288ff4ad254c
Yun Hong Min <wlqtk44@gmail.com>
2025-03-25 18:25:20 +0900

    03.25

--- Team1.Java_Mission/src/Java_Mission_Yoon/ClassRoom/_11_Bank/Bank.java
+++ Team1.Java_Mission/src/Java_Mission_Yoon/ClassRoom/_11_Bank/Bank.java
@@ -1,21 +1,25 @@
-package Java_Mission_Yoon.ClassRoom_11_Bank;
+package Java_Mission_Yoon.ClassRoom._11_Bank;
 
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bank {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		Account account = new Account();
 		
+		//TODO 
+//		ArrayList<String> personalInformation = new ArrayList<String>();
+		
 //		생성 순서
-		int index =0;
+		int index=0;
 		
 		// 비밀번호 배열
-		int[] pw = new int[index];
+		int[] pw = new int[1000];
 		
-		// 선택
 		
+		// 기본 출력 
 		do {
 			
 		System.out.println("================================");
@@ -27,22 +31,35 @@ public class Bank {
 		System.out.println("6. 종료");
 		System.out.println("================================");
 		
+		// 선택
+		System.out.print("입력>>");
 		int menuNo = sc.nextInt();
 		
 		switch (menuNo) {
 		case 1 : 
 			System.out.print("계좌 번호>>");
-			account.setAccountN(sc.nextLine());
+			account.setAccountNum(sc.nextLine());
+			sc.nextLine();
+			
 			System.out.print("예금주>>");
 			account.setAccountName(sc.nextLine());
+			
 			System.out.print("최초예금액>>");
 			account.setAccountMoney(sc.nextInt());
-			System.out.print("비밀번호");
+			
+			System.out.print("비밀번호>>");
 			pw[index] = sc.nextInt();
+			
+			System.out.println("'"+ account.getAccountName()+"' 님의 계좌가 개설되었습니다.");
+			
+			index++;
+			
+		
 			
 			
 		default:
 		}
+		sc.close();
 			
 		} while (true);
 		
